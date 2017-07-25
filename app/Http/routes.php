19fd88238e3886210a46eebf03be9264fc755e8a
Arius Lee <arius@Ariuss-MacBook-Pro.local>
2017-07-25 18:00:04 +0800

    Hotfix dependencies.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,11 +11,21 @@
 |
 */
 
-Route::get('/', 'WelcomeController@index');
+Route::get("/","LoginController@showLoginPage");
 
-Route::get('home', 'HomeController@index');
+Route::post("/login","LoginController@processLogin");
+Route::get("/login","LoginController@showLoginPage");
+Route::get("/logout","LoginController@processLogout");
 
-Route::controllers([
-	'auth' => 'Auth\AuthController',
-	'password' => 'Auth\PasswordController',
-]);
+Route::group(['middleware' => ['auth', 'roleAdmin'], 'prefix' => 'admin'], function () {
+
+    Route::get('/', 'AdminController@welcomeScreen');
+
+    Route::get('/broadband', 'TertiobroadbandController@index');
+
+    Route::get('/application-list', 'ApplicationController@listAllApplications');
+    Route::get('/application-search', 'ApplicationController@showSearchView');
+    Route::get('/application/delete/{applicationId}', 'ApplicationController@deleteApplicationById');
+    Route::post('/application/ajax-search-application', 'ApplicationController@getSearchResult');
+
+});
