aa097860a0fada877c14d063fa57e088fe945832
Arius Lee <arius@Ariuss-MacBook-Pro.local>
2017-07-25 23:25:19 +0800

    Remove header search.

--- resources/views/inc/header.blade.php
+++ resources/views/inc/header.blade.php
@@ -4,9 +4,9 @@
     <div class="navbar-header">
         <a class="navbar-minimalize minimalize-styl-2 btn btn-primary " href="#"><i class="fa fa-bars"></i> </a>
         <form role="search" class="navbar-form-custom" action="search_results.html">
-            <div class="form-group">
+            <!--<div class="form-group">
                 <input type="text" placeholder="Search for something..." class="form-control" name="top-search" id="top-search">
-            </div>
+            </div>-->
         </form>
     </div>
     <ul class="nav navbar-top-links navbar-right">
